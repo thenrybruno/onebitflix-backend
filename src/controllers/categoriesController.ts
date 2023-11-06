b7d3cee1a5b5e023f1f45055f4a46c1f64d87d12
Bruno Luiz <you@example.comthenrybruno@hotmail.com.br>
2023-11-06 13:48:38 -0300

    Obtendo cursos de uma categoria

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -3,6 +3,7 @@ import { categoryService } from "../services/categoryService";
 import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const categoriesController = {
+    //GET: busca todas as categorias no caminho /categories
     index: async (req: Request, res: Response) => {
         const [ page, perPage ] = getPaginationParams(req.query)
 
@@ -16,5 +17,19 @@ export const categoriesController = {
             }
         }
         
+    },
+
+    //GET: Busca uma categoria específica através de um id no caminho /categories/:id
+    show:async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        try {
+            const category = await categoryService.findByIdWithCourses(id)
+            return res.json(category)
+        } catch (error) {
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+        }
     }
 }
