dc363c4d94660b8a70f5c2cf3950a129f768f808
Bruno Luiz <you@example.comthenrybruno@hotmail.com.br>
2023-11-09 13:43:47 -0300

    Criação da rota para obter 3 cursos em destaque

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,6 +2,19 @@ import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 
 export const coursesController = {
+    // GET => /courses/featured
+    featured: async (req: Request, res: Response) => {
+
+        try {
+            const featuredCourses = await courseService.getRandomFeaturedCourses()
+            return res.json(featuredCourses)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
+    },
+
     // GET => /courses/:id
     show: async (req: Request, res: Response) => {
         const { id } = req.params
@@ -10,8 +23,8 @@ export const coursesController = {
             const course = await courseService.findByIdWithEpisodes(id)
             return res.json(course)
         } catch (error) {
-            if(error instanceof Error){
-                return res.status(400).json({message: error.message})
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
             }
         }
     }
