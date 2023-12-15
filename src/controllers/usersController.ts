8ba3a8bb9aa372bc5a80875b79dbe73f6ae4be9c
Bruno Luiz <you@example.comthenrybruno@hotmail.com.br>
2023-12-15 20:14:58 -0300

    Adicionando rota para alteração da senha

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -18,7 +18,7 @@ export const usersController = {
     //PUT => /users/current
     update: async (req: AuthenticatedRequest, res: Response) => {
         const { id } = req.user!
-        const { firstName, lastName, phone, email, birth} = req.body
+        const { firstName, lastName, phone, email, birth } = req.body
 
         try {
             const updatedUser = await userService.update(id, {
@@ -35,6 +35,27 @@ export const usersController = {
                 return res.status(400).json({ message: error.message })
             }
         }
+    },
+
+    //PUT => /users/current/password
+    updatePassword: async (req: AuthenticatedRequest, res: Response) => {
+        const user = req.user!
+        const { currentPassword, newPassword } = req.body
+        
+        user.checkPassword(currentPassword, async (err, isSame) => {
+            try {
+                if (err) return res.status(400).json({ message: err.message })
+                if (!isSame) return res.status(400).json({ message: 'Senha Incorreta.' })
+
+                await userService.updatePassword(user.id, newPassword)
+                return res.status(204).send()
+            } catch (error) {
+                if (error instanceof Error) {
+                    return res.status(400).json({ message: error.message })
+                }
+            }
+        })
+
     },
 
     //GET => /users/current/watching
